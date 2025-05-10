c7b57e0bd7836966fb3ae4b705af702299e5024a
Christopher-OOC <olojedechristopher24@gmail.com>
2025-05-10 21:44:52 +0100

    commit

--- src/App2.js
+++ src/App2.js
@@ -1,36 +1,40 @@
 import { useState } from "react";
 import "./index2.css";
 
-const products = [
+const initialProducts = [
   {
     id: 1,
     name: "Product 1",
-    image: "",
-    description: "",
+    image: "https://i.pravatar.cc/50?u=7654",
+    description:
+      "Nulla sit commodo incididunt irure ea ad. Nulla sit commodo incididunt irure ea ad",
     price: 12,
     rating: 7.5,
   },
   {
     id: 2,
     name: "Product 2",
-    image: "",
-    description: "",
+    image: "https://i.pravatar.cc/50?u=7954",
+    description:
+      "Aliqua et cupidatat elit aute esse. Nulla sit commodo incididunt irure ea ad.",
     price: 29,
     rating: 10.5,
   },
   {
     id: 3,
     name: "Product 3",
-    image: "",
-    description: "",
+    image: "https://i.pravatar.cc/50?u=2654",
+    description:
+      "Occaecat qui esse adipisicing duis nostrud consequat cupidatat.Dolore ex esse excepteur minim ullamco eiusmod dolor esse.",
     price: 32.9,
     rating: 4.8,
   },
   {
     id: 4,
     name: "Product 4",
-    image: "",
-    description: "",
+    image: "https://i.pravatar.cc/50?u=7656",
+    description:
+      "Anim ullamco Lorem sint magna occaecat qui esse adipisicing duis nostrud consequat cupidatat.",
     price: 7.9,
     rating: 24.9,
   },
@@ -39,6 +43,8 @@ const products = [
 export default function App2() {
   const [search, setSearch] = useState("");
   const [sortByRating, setSortByRating] = useState("");
+  const [products, setProducts] = useState(initialProducts);
+  const [searchProducts, setSearchProducts] = useState(initialProducts);
 
   function handleOnSearch(keyword) {
     setSearch(keyword);
@@ -49,6 +55,18 @@ export default function App2() {
     setSortByRating(rateOption);
     console.log(rateOption);
   }
+
+  function handleSearch(e, search, rating) {
+    e.preventDefault();
+    setSearchProducts([
+      ...products.filter(
+        (product) =>
+          product.name.toLowerCase().includes(search.toLowerCase()) ||
+          product.description.toLowerCase().includes(search.toLowerCase())
+      ),
+    ]);
+  }
+
   return (
     <div>
       <Header
@@ -56,13 +74,14 @@ export default function App2() {
         sortByRating={sortByRating}
         onSearch={handleOnSearch}
         onSort={handleOnSort}
+        handleSearch={handleSearch}
       />
-      <Content />
+      <Content searchProducts={searchProducts} />
     </div>
   );
 }
 
-function Header({ search, sortByRating, onSearch, onSort }) {
+function Header({ search, sortByRating, onSearch, onSort, handleSearch }) {
   return (
     <div className="header">
       <h1>Topnivo Ecommerce</h1>
@@ -71,12 +90,13 @@ function Header({ search, sortByRating, onSearch, onSort }) {
         sortByRating={sortByRating}
         onSearch={onSearch}
         onSort={onSort}
+        handleSearch={handleSearch}
       />
     </div>
   );
 }
 
-function Search({ search, sortByRating, onSearch, onSort }) {
+function Search({ search, sortByRating, onSearch, onSort, handleSearch }) {
   return (
     <div className="form__box">
       <form className="form__search">
@@ -101,9 +121,37 @@ function Search({ search, sortByRating, onSearch, onSort }) {
             <option value={2}>Greather than 10</option>
           </select>
         </div>
+        <button
+          type="submit"
+          onClick={(e) => handleSearch(e, search, sortByRating)}
+        >
+          Search
+        </button>
       </form>
     </div>
   );
 }
 
-function Content() {}
+function Content({ searchProducts }) {
+  return (
+    <div className="content">
+      <h2 className="content__title">Product Lists</h2>
+      <div className="product-list">
+        {searchProducts.map((product) => (
+          <Product product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Product({ product }) {
+  return (
+    <div className="product">
+      <img src={product.image} alt={product.name} className="product__image" />
+      <h2>Name: {product.name}</h2>
+      <p>Description: {product.description}</p>
+      <span>Rating: {product.rating}</span>
+    </div>
+  );
+}
